6033583d54eb90d1b3f12e76c488f47f90686153
Sandeep <sandeep.sajiru@gmail.com>
2015-12-09 09:48:50 +0530

    Class-2 Code

---
+++ Sandeep/Class-2/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void test()
+{
+    printf("%5d", 100);
+    printf("\n%5d", 1);
+    printf("\n%5d", 1000);
+
+    /*
+    while(!printf("Hello"))
+    {
+
+    }
+*/
+
+    /*
+    int n = printf("hello\n");
+    printf("%d", n);
+
+    n = 200;
+    printf("\nANS: %d", printf("N %d", n));
+    */
+    //return 0;
+}
+
+void printCalendar(int y, int m)
+{
+    // Print Month Name and Year
+
+}
+
+int main()
+{
+
+    printCalendar(2009, 2);
+    printCalendar(2014, 7);
+    printCalendar(2016, 2);
+    return 0;
+}
