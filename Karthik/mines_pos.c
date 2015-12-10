6b5c23c80c732d5b8a52f4525b71ce60e099e867
Home <k.karthik248@gmail.com>
2015-12-10 16:56:40 +0530

    mines_pos updated

--- Karthik/mines_pos.c
+++ Karthik/mines_pos.c
@@ -5,23 +5,22 @@ void generateAndPrintMinesweeperBoard(int m, int n, int mines)
 {
     int r,c,i,j;
     srand(time(NULL));
-    for(i=0;i<m;i++)
-        for(j=0;j<n;j++)
-            Board[i][j
-            ]=0;
+    for(i=1;i<=m;i++)
+        for(j=1;j<=n;j++)
+            Board[i][j]=0;
     for(i=0;i<mines;i++)
     {
-        r=rand()%m;
-        c=rand()%n;
+        r=rand()%m + 1;
+        c=rand()%n + 1;
         if(Board[r][c]==1)
             i--;
         else
             Board[r][c]=1;
     }
 
-    for(i=0;i<m;i++)
+    for(i=1;i<=m;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=1;j<=n;j++)
             printf("%d ",Board[i][j]);
         printf("\n");
     }
