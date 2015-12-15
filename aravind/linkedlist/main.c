a0440b2fac4cd7151db92e5b47a7d9b52abce84d
Aravind Vaddi <Aravind Vaddi>
2015-12-15 08:42:00 +0530

    Crash

--- aravind/linkedlist/main.c
+++ aravind/linkedlist/main.c
@@ -22,24 +22,89 @@ LLNode *insertInBegin(LLNode *h,int data)
     temp->next = h;
     return temp;
 }
-LLNode *insertInEnd(LLNode *h,int data);
+LLNode *findLastNode(LLNode *h)
+{
+    if(h==NULL)
+        return h;
+    while(h->next!=NULL)
+    {
+        h=h->next;
+    }
+    return h;
+}
+LLNode *insertInEnd(LLNode *h,int data)
+{
+    LLNode *l;
+    LLNode *temp = createLLNode(data);
+    if(h==NULL)
+        return temp;
+    l = findLastNode(data);
+    l->next= temp;
+    return h;
+}
+LLNode *deleteLLNodeWithValue(LLNode *h,int data)
+{
+    LLNode *previous, *current;
+    if(h==NULL)
+        return h;
+    previous = current = h;
+    if(current->data==data)
+    {
+        h=h->next;
+        free(current);
+        return h;
+    }
+    current = current->next;
+    while(current->next!=NULL)
+    {
+        if(current->data==data)
+        {
+            previous->next = current->next;
+            free(current);
+            return h;
+        }
+        else
+        {
+            current = current->next;
+            previous = previous->next;
+        }
+    }
+    if(current->next==NULL&&current->data==data)
+    {
+        previous->next=NULL;
+        free(current);
+        return h;
+    }
+    return h;
+}
+LLNode *createSerialList(int n)
+{
+    LLNode *h;
+    int i;
+    for(i=1;i<=n;i++)
+        h = insertInEnd(h, i);
+    return h;
+}
 void traverse(LLNode *h)
 {
     LLNode *t = h;
-    while(t->next!=NULL) //while(
+    while(t->next!=NULL)
     {
-        printf("%d ",t->date);
+        printf("%d ",t->data);
         t = t->next;
     }
 }
 int main()
 {
-    LLnode *head = NULL;
+
+    LLNode *head = NULL;
+    createSerialList(9);
+    /*
     head = insertInBegin(head, 5);
     head = insertInBegin(head, 4);
     head = insertInBegin(head, 3);
     head = insertInBegin(head, 2);
-    head = insertInBegin(head, 1);
+    head = insertInBegin(head, 1);*/
     traverse(head);
     return 0;
 }
