a6307bfd13d2d91e66b8999a382c3d61938c6ae4
Sandeep <sandeep.sajiru@gmail.com>
2015-12-23 07:44:30 +0530

    Adding BST Exercise

---
+++ Sandeep/BST - 1/BST.c
@@ -0,0 +1,22 @@
+#include "BST.h"
+#include <stdlib.h>
+
+BSTNode *createBSTNode(ElementType data)
+{
+   return NULL;
+}
+
+BSTNode *rinsertBSTNode(BSTNode *root, ElementType data)
+{
+    return NULL;
+}
+
+BSTNode *insertBSTNode(BSTNode *root, ElementType data)
+{
+    return NULL;
+}
+
+BSTNode *delteBSTNode(BSTNode *root, ElementType data)
+{
+    return NULL;
+}
