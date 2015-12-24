4950efe9a4acf8c38f9792a145ddf499cadf17ea
harsha vardhan g <harsha vardhan g>
2015-12-24 08:39:35 +0530

    -A

--- Sandeep/BST - 1/main.c
+++ Sandeep/BST - 1/main.c
@@ -15,11 +15,22 @@ int main()
     root = insertBSTNode(root, 9);
 
     print_ascii_tree(root);
+    root = rinsertBSTNode(root, 5);
 
-    // Delete Root
-    root = delteBSTNode(root, 5);
+    root = rinsertBSTNode(root, 3);
+    root = rinsertBSTNode(root, 1);
+    root = rinsertBSTNode(root, 4);
 
+    root = rinsertBSTNode(root, 7);
+    root = rinsertBSTNode(root, 6);
+    root = rinsertBSTNode(root, 9);
     print_ascii_tree(root);
+
+    //BSTNode *t=searchBSTNode(root,10);
+    // Delete Root
+    // root = delteBSTNode(root, 5);
+
+
 
     return 0;
 }
