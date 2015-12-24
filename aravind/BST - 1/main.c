5495509cdfa71193288f09635aade8f034ca0fbf
Aravind Vaddi <Aravind Vaddi>
2015-12-24 08:19:59 +0530

    anothercommit

--- aravind/BST - 1/main.c
+++ aravind/BST - 1/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "BST.h"
-int main()
+int insertUnitTest()
 {
     BSTNode *root = NULL;
     root = insertBSTNode(root, 5);
@@ -16,11 +16,38 @@ int main()
 
     print_ascii_tree(root);
 
+    root = deleteBSTNode(root, 5);
+
+    print_ascii_tree(root);
+
+    return 0;
+}
+int rinsertUnitTest()
+{
+    BSTNode *root = NULL;
+    root = rinsertBSTNode(root, 5);
+
+    root = rinsertBSTNode(root, 3);
+    root = rinsertBSTNode(root, 1);
+    root = rinsertBSTNode(root, 4);
+
+    root = rinsertBSTNode(root, 7);
+    root = rinsertBSTNode(root, 6);
+    root = rinsertBSTNode(root, 9);
+
+    print_ascii_tree(root);
+
     // Delete Root
     /*
     root = delteBSTNode(root, 5);
 
     print_ascii_tree(root);
 */
+    return 0;
+}
+int main()
+{
+    insertUnitTest();
+    rinsertUnitTest();
     return 0;
 }
