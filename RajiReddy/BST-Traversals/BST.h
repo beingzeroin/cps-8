548b5f0d545a2600cd9a35d20abdedf38de39f4c
RajiReddy <iamrajireddy@gmail.com>
2015-12-24 09:30:36 +0530

    Buggy Traversal - PReorder

---
+++ RajiReddy/BST-Traversals/BST.h
@@ -0,0 +1,24 @@
+#ifndef BST_H_INCLUDED
+#define BST_H_INCLUDED
+
+typedef int ElementType;
+typedef struct bstNode BSTNode;
+struct bstNode
+{
+    ElementType data;
+    BSTNode *left;
+    BSTNode *right;
+};
+
+BSTNode *createBSTNode(ElementType data);
+BSTNode *insertBSTNode(BSTNode *root, ElementType data);
+BSTNode *rinsertBSTNode(BSTNode *root, ElementType data);
+BSTNode *searchBSTNode(BSTNode *root, ElementType data);
+BSTNode *deleteBSTNode(BSTNode *root, ElementType data);
+BSTNode *rdeleteBSTNode(BSTNode *root,ElementType data);
+void print_ascii_tree(BSTNode * t);
+
+void iterativePreorder(BSTNode *r);
+void recursivPreorder(BSTNode *r);
+
+#endif // BST_H_INCLUDED
