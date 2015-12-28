28b7c7d7dcc6a995c11bd109ec83c6c24329ddfe
Pavan <ypk507@gmail.com>
2015-12-28 07:54:22 +0530

    -Amyfinal

--- pavan507/BST-Traversals/Traversals.cpp
+++ pavan507/BST-Traversals/Traversals.cpp
@@ -1,29 +1,235 @@
 #include <stack>
+#include "BST.h"
 #include<stdlib.h>
 #include<stdio.h>
-#include "BST.h"
+#include<queue>
+int c=0;
 using namespace std;
 
 void iterativeInorder(BSTNode *r)
 {
+    stack<BSTNode*> mystack;
     if(r==NULL)
         return;
-    stack<BSTNode*> mystack;
-while(1)
-{
-    while(r!=NULL)
+    while(1)
     {
-        mystack.push(r);
-        r=r->left;
+        while(r!=NULL)
+        {
+            mystack.push(r);
+            r=r->left;
+
+
+        }
+        if(mystack.empty())
+            break;
+        r=mystack.top();
+        mystack.pop();
+        printf("%d ",r->data);
+        r=r->right;
+
 
     }
-    if(mystack.empty())
-    break;
-    r=mystack.top();
+
+}
+void recursiveInorder(BSTNode* r)
+{
+    if(r==NULL)
+        return;
+    recursiveInorder(r->left);
     printf("%d ",r->data);
-    mystack.pop();
-    r=r->right;
+    recursiveInorder(r->right);
+}
+void iterativePostorder(BSTNode *r)
+{
+    BSTNode *p=NULL,*rc=NULL;
+    if(r==NULL)
+        return;
+    stack<BSTNode*>mystack;
+    while(1)
+    {
+        while(r!=NULL)
+        {
+            if(r->right!=NULL)
+                mystack.push(r->right);
+            mystack.push(r);
+            r=r->left;
+        }
+        if(mystack.empty())
+            break;
+        p=mystack.top();
+        mystack.pop();
+        if(mystack.empty())
+        {
+            printf("%d ",p->data);
+            break;
+
+        }
+        rc=mystack.top();
+        mystack.pop();
+        if(p->right==rc)
+        {
+            mystack.push(p);
+            r=rc;
+        }
+        else
+        {
+            printf("%d ",p->data);
+            mystack.push(rc);
+            r=NULL;
+
+        }
+
+    }
+
+}
+void levelOrderTraversal(BSTNode *r)
+{
+    queue<BSTNode*>myqueue;
+    if(r==NULL)
+        return;
+    myqueue.push(r);
+    while(!myqueue.empty())
+    {
+        r=myqueue.front();
+        myqueue.pop();
+        printf("%d ",r->data);
+        if(r->left!=NULL)
+            myqueue.push(r->left);
+        if(r->right!=NULL)
+            myqueue.push(r->right);
+
+    }
+
+}
+int height(BSTNode *r)
+{
+    if(r==0)
+        return 0;
+    int lheight=height(r->left);
+    int rheight=height(r->right);
+    if(lheight>rheight)
+        return 1+lheight;
+    else
+        return 1+rheight;
 
 }
 
+void printLevelOrder(BSTNode *r)
+{
+    int h=height(r);
+    int i;
+    for(i=1;i<=h;i++)
+    {
+
+        printGivenLevel(r,i);
+    }
+
 }
+void printGivenLevel(BSTNode *r,int level)
+{
+    if(r==NULL)
+        return;
+    if(level==1)
+        printf("%d ",r->data);
+    else if(level>1)
+    {
+
+        printGivenLevel(r->left,level-1);
+        printGivenLevel(r->right,level-1);
+    }
+}
+
+void printSpiralOrder(BSTNode *r)
+{
+    int h=height(r);
+    int i,flag=0;
+    for(i=1;i<=h;i++)
+    {
+
+        printSpiralLevel(r,i,flag);
+        if(flag==0)flag=1;
+        else flag=0;
+    }
+
+}
+void printSpiralLevel(BSTNode *r,int level,int flag)
+{
+    if(r==NULL)
+        return;
+    if(level==1)
+        printf("%d ",r->data);
+    else if(level>1)
+    {
+       if(flag==0)
+       {
+        printSpiralLevel(r->left,level-1,flag);
+        printSpiralLevel(r->right,level-1,flag);
+    }
+    else{
+        printSpiralLevel(r->right,level-1,flag);
+        printSpiralLevel(r->left,level-1,flag);
+    }
+}
+}
+int nodeCount(BSTNode *r)
+{
+    if(r==NULL)
+        return 0;
+    return (1+nodeCount(r->left)+nodeCount(r->right));
+
+}
+/*
+void spiralOrderTraversal(BSTNode *r)
+{
+    int i,k=0;
+    queue<BSTNode*>myqueue;
+    if(r==NULL)
+        return;
+    myqueue.push(r);
+    c++;
+    i=1;
+    while(!myqueue.empty())
+    {
+        while(c--)
+        {
+            r=myqueue.front();
+            myqueue.pop();
+            printf("%d ",r->data);
+            if(i%2==0)
+            {
+                if(r->right!=NULL)
+                {
+                    myqueue.push(r->right);
+                    k++;
+                }
+                if(r->left!=NULL)
+                {
+                    myqueue.push(r->left);
+                    k++;
+                }
+
+
+            }
+            else
+            {
+                if(r->left!=NULL)
+                {
+                    myqueue.push(r->left);
+                    k++;
+                }
+                if(r->right!=NULL)
+                {
+                    myqueue.push(r->right);
+                    k++;
+                }
+            }
+
+        }
+        c=k;
+        k=0;
+        i++;
+
+
+    }
+
+}*/
--- pavan507/BST-Traversals/main.cpp
+++ pavan507/BST-Traversals/main.cpp
@@ -3,22 +3,46 @@
 #include "BST.h"
 #include<assert.h>
 
-int unitTests()
+
+int main()
 {
-    BSTNode *root = NULL;
-    root = insertBSTNode(root, 5);
-    root = insertBSTNode(root, 3);
-    root = insertBSTNode(root, 1);
-    root = insertBSTNode(root, 4);
+    BSTNode *root1 = NULL,*root2=NULL,*root=NULL;
+    root1 = insertBSTNode(root1, 5);
+    root1 = insertBSTNode(root1, 3);
+    root1 = insertBSTNode(root1, 1);
+    root1= insertBSTNode(root1, 4);
 
-    root = insertBSTNode(root, 7);
-    root = insertBSTNode(root, 6);
-    root = insertBSTNode(root, 9);
+    root1 = insertBSTNode(root1, 7);
+    root1 = insertBSTNode(root1, 6);
+    root1 = insertBSTNode(root1, 9);
+    root1=insertBSTNode(root1,14);
     //assert(searchBSTNode(root,3)!=NULL);
+root2=insertBSTNode(root2,13);
+root2=insertBSTNode(root2,19);
+root2=insertBSTNode(root2,11);
+root=insertBSTNode(root,8);
+/*root->left=root2;
+root->right=root1;*/
+    print_ascii_tree(root1);
+    //printf("%d ",nodeCount(root1));
+    //printf("\n%d ",height(root1));
+  // printLevelOrder(root1);
+    printf("\n");
+   //root1= deleteBSTNode(root1,7);
+    //print_ascii_tree(root1);
+    printf("%d ",hasSum(root1,6));
+    //printSpiralOrder(root1);
+    //levelOrderTraversal(root);
+    //printf("\n%d \n",height(root));
 
-    print_ascii_tree(root);
-    iterativeInorder(root);
-
+    //if(isBST(root1))
+     //   printf("bst");
+    //else
+    //printf("not bst");
+    //printf("\n spiral order\n");
+//    spiralOrderTraversal(root);
+  //  iterativePostorder(root);
+    //recursiveInorder(root);
     // Delete Root
    /* root = rdeleteBSTNode(root, 5);
     print_ascii_tree(root);
@@ -28,13 +52,9 @@ int unitTests()
     print_ascii_tree(root);
     root = rdeleteBSTNode(root, 100);
 
-    print_ascii_tree(root);
-*/
-    return 0;
-}
-
-int main()
-{
+    print_ascii_tree(root);*/
 
     return 0;
 }
+
+
