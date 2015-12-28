a345da350cc3ad51f1e0573883eac37ddefe675a
Sandeep <sandeep.sajiru@gmail.com>
2015-12-29 00:18:31 +0530

    Class-15 Code

--- Sandeep/BST-Traversals/BST.h
+++ Sandeep/BST-Traversals/BST.h
@@ -1,5 +1,6 @@
 #ifndef BST_H_INCLUDED
 #define BST_H_INCLUDED
+#include <stdbool.h>
 
 typedef int ElementType;
 typedef struct bstNode BSTNode;
@@ -18,15 +19,11 @@ BSTNode *searchBSTNode(BSTNode *root, ElementType data);
 BSTNode *deleteBSTNode(BSTNode *root, ElementType data);
 BSTNode *rdeleteBSTNode(BSTNode *root,ElementType data);
 void print_ascii_tree(BSTNode * t);
-<<<<<<< HEAD
-void iterativePreorder(BSTNode *r);
-void iterativeInorder(BSTNode *r)
-=======
-
 void iterativePreorder(BSTNode *r);
 void recursivPreorder(BSTNode *r);
 void iterativeInorder(BSTNode *r);
 void iterativePostrder(BSTNode *r);
+bool hasSum(BSTNode *r, int sum);
+void levelOrderSpiral(BSTNode *r);
 
->>>>>>> fe043da394043d1e9fd8d5058ed0e4e03257dc63
 #endif // BST_H_INCLUDED
