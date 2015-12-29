738ad0a139076de8d12c0d3754da78bba3a1c806
sanjaykatta95 <sanjay18495@gmail.com>
2015-12-29 07:53:50 +0530

    second

--- Sandeep/BST - 1/BST.c
+++ Sandeep/BST - 1/BST.c
@@ -3,20 +3,114 @@
 
 BSTNode *createBSTNode(ElementType data)
 {
-   return NULL;
+   BSTNode *node=malloc(sizeof(BSTNode));
+   node->data=data;
+   node->left=NULL;
+   node->right=NULL;
+   return node;
 }
 
 BSTNode *rinsertBSTNode(BSTNode *root, ElementType data)
 {
-    return NULL;
+    if(root==NULL)
+        return createBSTNode(data);
+    if(root->data>data)
+        root->left = rinsertBSTNode(root->left,data);
+    if(root->data<data)
+        root->right=rinsertBSTNode(root->right,data);
+    return root;
 }
 
 BSTNode *insertBSTNode(BSTNode *root, ElementType data)
 {
+    if(root==NULL)
+        return createBSTNode(data);
+        BSTNode *c=root;
+    while(1)
+    {
+        if(c->data>data)
+    {
+        if(c->left==NULL)
+        {
+            c->left=createBSTNode(data);
+            return root;
+        }
+        c=c->left;
+    }
+    if(c->data<data)
+    {
+        if(c->right==NULL)
+        {
+            c->right=createBSTNode(data);
+            return root;
+        }
+        c=c->right;
+    }
+    }
     return NULL;
 }
 
-BSTNode *delteBSTNode(BSTNode *root, ElementType data)
+BSTNode *searchBSTNode(BSTNode *root,ElementType data)
 {
-    return NULL;
+        if(root==NULL)
+            return NULL;
+        if(root->data==data)
+            return root;
+        if(root->data>data)
+            return searchBSTNode(root->left,data);
+        if(root->data<data)
+            return searchBSTNode(root->right,data);
+            return NULL;
+}
+ElementType min(BSTNode *n)
+{
+    while(n->left!=NULL)
+        n=n->left;
+    return n->data;
+}
+BSTNode *rdeleteBSTNode(BSTNode *root,ElementType data)
+{
+    if(root->data == data)
+    {
+        if(root->left == NULL && root->right == NULL)
+        {
+            free(root);
+            return NULL;
+        }
+        else if(root->left!=NULL && root->right!=NULL)
+        {
+            BSTNode *c= root->right;
+            while(c->left!=NULL)
+                c=c->left;
+            root->data = c->data;
+            root->right = deleteBSTNode(root->right,c->data);
+
+
+        }
+        else
+        {
+
+            if(root->left!=NULL)
+            {
+                BSTNode *c;
+
+                c = root->left;
+                free(root);
+                return c;
+            }
+            else
+            {
+                BSTNode *c;
+                c = root->right;
+                free(root);
+                return c;
+            }
+        }
+    }
+    else if(data <root->data)
+        root->left = deleteBSTNode(root->left, data);
+    else
+        root->right = deleteBSTNode(root->right, data);
+
+    return root;
 }
--- Sandeep/BST - 1/main.c
+++ Sandeep/BST - 1/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "BST.h"
+#include <assert.h>
 int main()
 {
     BSTNode *root = NULL;
+    /*root = rinsertBSTNode(root, 5);
+
+    root = rinsertBSTNode(root, 3);
+    root = rinsertBSTNode(root, 1);
+    root = rinsertBSTNode(root, 4);
+
+    root = rinsertBSTNode(root, 7);
+    root = rinsertBSTNode(root, 6);
+    root = rinsertBSTNode(root, 9);*/
+
     root = insertBSTNode(root, 5);
 
     root = insertBSTNode(root, 3);
@@ -17,9 +28,22 @@ int main()
     print_ascii_tree(root);
 
     // Delete Root
-    root = delteBSTNode(root, 5);
+
 
     print_ascii_tree(root);
+
+    BSTNode *temp=searchBSTNode(root,3);
+    assert(temp!=NULL&&temp->data==3);
+    if(temp==NULL)
+    {
+
+    }
+    else
+        printf("%d",temp->data);
+
+        root = rdelteBSTNode(root, 5);
+        print_ascii_tree(root);
+
 
     return 0;
 }
