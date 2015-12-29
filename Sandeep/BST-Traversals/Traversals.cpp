8dd688035aafd2a04f7812864a8ab0fbf266fc91
Vineeth <vineethpenchala1010@gmail.com>
2015-12-29 07:53:31 +0530

    msg

--- Sandeep/BST-Traversals/Traversals.cpp
+++ Sandeep/BST-Traversals/Traversals.cpp
@@ -5,6 +5,40 @@ using namespace std;
 void iterativePreorder(BSTNode *r)
 {
     stack<BSTNode*> mystack;
+     Stack *s=createStack();
+     while(1)
+     {
+         while(r!=NULL)
+         {
+
+             printf("%d",r->data);
+             mystack.push(r);
+             r=r->left;
+         }
+         if(myStack.empty())
+            break;
+         r=myStack.top();
+         myStack.pop();
+         r=r->right;
+     }
+
+}
+void iterativeInorder(BSTNode *r)
+{
+    stack<BSTNode*> mystack;
+    Stack *s=createStack();
+    while(1)
+    {
+       while(r!=NULL)
+       {
+        mystack.push(r);
+
+
+       }
+
+    }
+
+
 
 
 }
