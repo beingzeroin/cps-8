20dd9a92ca21dff0a18c1ad96562cafd83bef70a
Karthik <k.karthik248@gmail.com>
2015-12-29 09:24:47 +0530

    cls-16

--- Karthik/trie/trie.h
+++ Karthik/trie/trie.h
@@ -2,14 +2,18 @@
 #define TRIE_H_INCLUDED
 
 #define ALPHABET_SIZE 26
-#incluse <stdlib.h>
+#include <stdlib.h>
 typedef struct trien TNode;
 struct trien
 {
     bool isEOW;
     TNode *next[ALPHABET_SIZE];
 };
-
+struct trien *createTrieNode();
+bool rInsertWordInTrie(TNode *root,char word[]);
+bool insertWordInTrie(TNode *root,char *word);
+void printWordsInTrie(TNode *root);
+bool deleteWordFromTrie(TNode *root,char *word);
 
 
 #endif // TRIE_H_INCLUDED
